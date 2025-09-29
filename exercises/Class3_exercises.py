03b0778d8008ebeac3603b3de97e40077b6fd7d1
Maria Alexandra Oliveira <maro.99799@gmail.com>
2025-09-29 19:29:39 +0100

    Content for class 3

---
+++ exercises/Class3_exercises.py
@@ -0,0 +1,156 @@
+'''
+EXERCISES FOR CLASS 3
+DATA VISUALIZATION
+'''
+
+'''
+Exercise 1
+
+Plot a simple line chart of y = [1, 3, 2, 4].
+'''
+# ANSWER
+
+'''
+Exercise 2
+
+Add markers to the line chart from Exercise 1.
+'''
+# ANSWER
+
+'''
+Exercise 3
+
+Create a bar chart with categories A, B, C, D and values [4, 7, 2, 9].
+'''
+# ANSWER
+
+'''
+Exercise 4
+
+Create a histogram of 100 random values drawn from a normal distribution.
+'''
+# ANSWER
+
+'''
+Exercise 5
+
+Create a scatter plot of x = [5,7,8,7,2,17,2,9,4,11,12,9,6] and
+y = [99,86,87,88,100,86,103,87,94,78,77,85,86].
+'''
+# ANSWER
+
+'''
+Exercise 6
+
+Add axis labels and a title to the scatter plot from Exercise 5.
+'''
+# ANSWER
+
+'''
+Exercise 7
+
+Create a subplot with two plots: 
+(1) line chart of y=[3,8,1,10] 
+(2) bar chart of categories A,B,C,D with values [3,8,1,10].
+'''
+# ANSWER
+
+'''
+Exercise 8
+
+Use seaborn to create a histogram of the "total_bill" column
+How to get the dataset: sns.load_dataset("tips"). Store it into a variable so
+you can reuse it in other exercies
+'''
+# ANSWER
+
+'''
+Exercise 9
+
+Use seaborn to create a scatterplot of total_bill vs tip from tips dataset.
+'''
+# ANSWER
+
+'''
+Exercise 10
+
+Add the hue parameter to Exercise 9 using the column "sex".
+'''
+# ANSWER
+
+'''
+Exercise 11
+
+Create a boxplot of total_bill by day from the tips dataset.
+'''
+# ANSWER
+
+'''
+Exercise 12
+
+Create a violinplot of tip by day, split by sex.
+'''
+# ANSWER
+
+'''
+Exercise 13
+
+Create a pairplot of the tips dataset, using hue="time".
+'''
+# ANSWER
+
+'''
+Exercise 14
+
+Plot two lines on the same chart: 
+y1 = [3,8,1,10] and y2 = [6,2,7,11].
+Add a legend.
+'''
+# ANSWER
+
+'''
+Exercise 15
+
+Save a simple bar chart (categories A,B,C with values [5,6,7]) 
+to a file named "barplot.png".
+'''
+# ANSWER
+
+'''
+Exercise 16
+
+Using the tips dataset, create a barplot of average tip by day.
+'''
+# ANSWER
+
+'''
+Exercise 17
+
+Create a seaborn lineplot of tip vs size, using hue="sex".
+'''
+# ANSWER
+
+'''
+Exercise 18
+
+Customize a seaborn plot with a matplotlib command: 
+Add a title "Tips Distribution" to a histogram of total_bill.
+'''
+# ANSWER
+
+'''
+Exercise 19
+
+Load the iris dataset (sns.load_dataset("iris")).
+Store it into a variable so you can reuse it in other exercies
+Plot a scatterplot of sepal_length vs sepal_width with hue="species".
+'''
+# ANSWER
+
+'''
+Exercise 20
+
+Using the iris dataset, create a pairplot with hue="species". 
+Save it as "iris_pairplot.png".
+'''
+# ANSWER
